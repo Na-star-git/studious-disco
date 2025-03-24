af570c973c3e17c317f33f33904febd888cd53a4
Na-star-git <nac03dk@sfr.fr>
2025-03-24 13:34:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,16 @@ def home():
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))  # Récupère le port défini par Render, sinon utilise 5000
     app.run(host="0.0.0.0", port=port, debug=False)
+
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")  # Assure-toi que index.html existe bien
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
